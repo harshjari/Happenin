4422f2757cdfd651bc9ef2654944e548a50254c4
Louis de Valliere <louis.de.valliere@gmail.com>
2012-06-10 03:48:57 -0700

    checked in

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,34 @@
+var client = {
+	getTweets : function(){
+		$.ajax({
+  			url: "/twitterfeed",
+  			cache: false	
+		}).done(function( json ) {
+			$.each(json, function(index, tweet) { 
+  				// alert(index + ': ' + value); 
+  				if( index < 30 ){
+	  				client.addBox(tweet);
+  				} 
+			});
+		});
+	},
+
+	addBox: function(tweet){
+		console.log(tweet)
+		var img = "";
+		
+		if(tweet.pic_num > 0){
+			img = '<img src="' + tweet.picture[0] + ' />';
+		}
+
+		var $boxes = $('<div class="item">' + img + ' ' + tweet.name + ' Tweet content.</div>');
+		$('.container').prepend( $boxes ).masonry( 'reload' );
+	}
+}
+
 $(function(){
+
+	
 	var $container = $('.container');
 
 	$container.imagesLoaded( function(){
@@ -6,4 +36,6 @@ $(function(){
     	itemSelector : '.item'
   		});
 	});
+
+	client.getTweets();
 });
