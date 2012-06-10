abcfe4423d06cbbf2a8e09cb781175130e80eb1f
Louis de Valliere <louis.de.valliere@gmail.com>
2012-06-09 20:47:14 -0700

    added twitterfeed

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require(__dirname + '/routes')
+  , util = require('util')
+  , twitter = require('twit');
 
 var app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -20,17 +21,59 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+// Twitter Client
+var twitter_client = new twitter({
+    consumer_key: 'hFoKKrU6yJuKGRA1TCzGwA',
+    consumer_secret: '2orp8MrqK0pxSrSsk1o2Pi5Kps533bzGniIQyeSTE',
+    access_token: '604028730-kf6vDQ4SVVclB1sXVmSboTr9ZfXxAHEiEqmdLl6J',
+    access_token_secret: 'WxmkvAiC2alyYpqLh2uAfHDByyJo4RbMxx9LFRC8Y'
+});
+
+/*app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
+*/
+
+// setInterval(
+//   function(){ twitter_client.search('#XHack2012', function(data) {
+//     console.log("INTERV"+util.inspect(data));
+//     })
+// }, 1000);
 
 // Routes
+app.get('/', function(req, res){
+  res.render('index', { title: 'happenin' });
+});
 
-app.get('/', routes.index);
+app.get('/index', function(req, res){
+  res.render('index' , { title: 'happenin' });
+});
+
+app.get('/twitterfeed', function(req,res){
+  var stream = twitter_client.stream('statuses/filter', { track: 'hack' });
+  stream.on('tweet', function (tweet) {
+    //console.log(tweet);
+    console.log(tweet.id_str);
+    if(tweet.entities.urls.length)
+      console.log("length" + tweet.entities.urls.length);
+    console.log("fasfasdalknwngbijbkajs"+tweet.entities.urls[0].url);
+    res.send(tweet);
+  });
+});
+
+// OAuth request according http://via.me/developers/authentication
+app.get('/getoauth', function(req, res){
+  res.render('getoauth' , { title: 'happenin' });
+})
+
+app.get('/callback_via_me', function(req, res){
+  res.render('index' , { title: 'happenin' });
+});
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
